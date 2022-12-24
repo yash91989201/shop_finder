39eb6856da82e0831c45856963e29e671727bd75
yash91989201 <yashraj6370@gmail.com>
2022-12-24 13:38:11 +0530

    fix fetch origin in client

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,6 +1,6 @@
 const api_baseurl =
   process.env.NODE_ENV === "production"
-    ? "http://localhost:80/api/v1"
-    : "http://localhost:3000/api/v1";
+    ? "http://127.0.0.1:80/api/v1"
+    : "http://127.0.0.1:3000/api/v1";
 
 export { api_baseurl };
